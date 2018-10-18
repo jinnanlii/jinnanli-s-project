ba0383b6f8eedda9200845f03170ef08b4d876dc
jinnanlii <44249108+jinnanlii@users.noreply.github.com>
2018-10-17 22:44:54 -0400

    Add files via upload

---
+++ solutions.py
@@ -0,0 +1,10 @@
+def latin(inputword):
+	lista=[]
+	listb=inputword.split()
+	if type(inputword)!=str:
+		return 'Invaild Input'
+	if len(listb)==0:
+		return inputword
+	else:
+		for items in listb:
+			items
